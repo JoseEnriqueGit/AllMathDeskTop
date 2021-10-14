e8dfeab7469ece052f091aae4197a564f504f7e9
Enrique <Enriquemk51@gmail.com>
2021-10-14 17:14:05 -0400

    Agregar archivos de proyecto.

---
+++ AllMath/Funciones/Formula.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AllMath.Funciones
+{
+    class Class1
+    {
+        public int Angulo(int Frecuencia, int TotalFrecuencia)
+        {
+            return 360 * Frecuencia/TotalFrecuencia;
+        }
+    }
+}
